99636d1e7643d498cc2e87bbf970683cac64c73f
Louis Angelini <lllouis@yahoo.com>
2014-01-06 05:22:38 +0000

    Change from nokogiri to rexml

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,10 +1,17 @@
 class MainPageController < ApplicationController
 
-	require 'open-uri'
+	require 'net/http'
+
+  require 'rexml/document'
 
 	def fetch
-		@trains_info = Nokogiri::XML(open('http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML'))
-		
-		@parse_trains_info_all = @trains_info.at('[text()= "Lisburn"]').parent.text
+		rail_url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+    
+    @xml_data = Net::HTTP.get_response(URI.parse(rail_url)).body
+    @doc = REXML::Document.new(@xml_data)
+    @station = []
+    @doc.elements.each('ArrayOfObjTrainPositions/objTrainPositions/TrainLatitude') do |a|
+    	@station << a.text
+    end
 	end
 end
