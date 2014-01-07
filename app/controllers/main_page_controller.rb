6cf46220f3e4af73cddd6a36bed73e129d70e454
Louis Angelini <lllouis@yahoo.com>
2014-01-07 00:30:48 +0000

    Crate select one page and select correct station

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -4,21 +4,45 @@ class MainPageController < ApplicationController
 
   require 'rexml/document'
 
-	def fetch
-		rail_url = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML"
+  before_filter :get_station_info, :only => [:fetch, :all, :select_one]
+
+  def get_station_info
+    rail_url = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML"
     
     @xml_data = Net::HTTP.get_response(URI.parse(rail_url)).body
     @doc = REXML::Document.new(@xml_data)
     @allStations = {} 
-    root = @doc.root
-    @stations = @doc.elements.each('ArrayOfObjStation/objStation') do |name|
-    	hash = {}
-    	hash[name.elements['StationDesc'].text] = {lat: name.elements['StationLatitude'].text, lon: name.elements['StationLongitude'].text}
-    	 
-    	@allStations.merge!(hash)
+    #root = @doc.root
+    @doc.elements.each('ArrayOfObjStation/objStation') do |name|
+      hash = {}
+      hash[name.elements['StationDesc'].text] = {lat: name.elements['StationLatitude'].text, lon: name.elements['StationLongitude'].text}
+       
+      @allStations.merge!(hash)
     end
+  end
+
+	def fetch
+		
+    
     @convert = @allStations.assoc('Belfast Central')
     @station = @convert.to_json
   end
 	
+  def all
+    
+    @stations = []
+
+    @doc.elements.each('ArrayOfObjStation/objStation') do |name|
+      @stations << name.elements['StationDesc'].text
+    end
+    @stations.sort!
+  end
+
+  def select_one
+    #= params[:data]
+
+
+
+    @returned_station = @allStations.assoc(params[:data]).to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 XMLParser::Application.routes.draw do
-  get 'get_info'      => 'main_page#fetch', :as => 'get_info'
-  get 'all_stations'  => 'main_page#all',   :as => 'all_stations'  
+  get 'get_info'      => 'main_page#fetch',       :as => 'get_info'
+  get 'all_stations'  => 'main_page#all',         :as => 'all_stations'  
+  get 'station_info'  => 'main_page#select_one',  :as=> 'station_info'
 
   root :to  => 'main_page#fetch'
 end
