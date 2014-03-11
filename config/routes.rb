4eaab2c2271cee77ec5bc4a72a1c8dd1da455b3e
Louis Angelini <lllouis@yahoo.com>
2014-03-11 11:36:33 +0000

    Add Favourites controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 XMLParser::Application.routes.draw do
 
+  get "favourites/destroy"
+
   get 'login'             => "sessions#new",                :as => 'login'
   get 'logout'            => 'sessions#destroy',            :as => 'logout'
   get 'signup'            => 'users#new',                   :as => 'signup'
